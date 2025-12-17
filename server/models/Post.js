5e9de721b87273117664ab39f5f0ab09ee076a88
ArielEstrada88 <aest4983@gmail.com>
2025-12-16 18:28:58 -0700

    initial commit

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
-const PostSchema = new Schema({
+const postSchema = new Schema({
   title: {
     type: String,
     required: true,
@@ -20,4 +20,4 @@ const PostSchema = new Schema({
   },
 });
 
-module.exports = mongoose.model("Post", PostSchema);
+module.exports = mongoose.model("Post", postSchema);
--- server/routes/main.js
+++ server/routes/main.js
@@ -4,17 +4,93 @@ const Post = require("../models/Post")
 
 // Home Page
 router.get("/", async (req, res)=> {
+  try { 
     const locals = {
-        title: "NodeJS Blog",
-        description: "A Blog template application that will be used for your own use.",
+      title: "NodeJS Blog",
+      description: "A Blog template application that will be used for your own use.",
     };
+ 
+    let perPage = 3;
+    let page = Math.max(parseInt(req.query.page) || 1, 1);
 
-    try {
-      const data = await Post.find().sort({createAt: "desc"});
-      res.render("index", { locals, data });
-    } catch (error) {
-      console.log(error);
-    }
+    const [data, count] = await Promise.all([
+      Post.find()
+        .sort({ createdAt: -1 })
+        .skip(perPage * (page - 1))
+        .limit(perPage),
+      Post.countDocuments({}),
+    ]);
+
+    const nextPage = page + 1;
+    const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+    res.render("index", { 
+      locals, 
+      data,
+      current: page,
+      nextPage: hasNextPage ? nextPage : null,
+      prevPage: page > 1 ? page - 1 : null,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+router.get("/about", (req, res) => {
+  const locals = {
+    title: "About",
+    description: "Learn more about the developer and this blog.",
+  };
+  res.render("about", { locals });
+});
+
+router.get("/contact", (req, res) => {
+  const locals = {
+    title: "contact",
+    description: "Contact info coming soon.",
+  };
+  res.render("contact", { locals });
+});
+
+// Get post by ID
+router.get("/post/:id", async (req, res) => {
+  try {
+    let slug = req.params.id;
+    const data = await Post.findById({ _id: slug });
+    const locals = {
+      title: data.title,
+      description:
+        "A blog template application that will be used for your own use.",
+    };
+    res.render("post", { locals, data });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// Search route
+router.post("/search", async (req, res) => {
+  try {
+    const locals = {
+      title: "Search",
+      description: "A blog template made with NodeJS and ExpressJS",
+    };
+    let searchTerm = req.body.SearchTerm;
+    const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9 ]/g, "");
+
+    const data = await Post.find({
+      $or: [
+        { title: { $regex: new RegExp(searchNoSpecialChar, "i") } },
+        { body: { $regex: new RegExp(searchNoSpecialChar, "i") } },
+      ],
+    }).sort({ createdAt: -1 });
+    res.render("search", { locals, data });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 module.exports = router;
